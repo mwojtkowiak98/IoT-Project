45110c8f50c230557265470ba201a64b7c13ea3a
Marcin W <marcinwojtkowiak2@gmail.com>
2021-06-21 18:37:24 +0200

    Init new repo

---
+++ FoodOrderApp/FoodOrderApp/Views/MenuPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodOrderApp.Views
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNext_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new OrderAddressPage());
+        }
+    }
+}
---
+++ FoodOrderApp/FoodOrderApp/Views/OrderAddressPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodOrderApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrderAddressPage : ContentPage
+    {
+        public OrderAddressPage()
+        {
+            InitializeComponent();
+        }
+
+        CancellationTokenSource cts;
+
+        private async Task GetCurrentLocation(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                if (location != null)
+                {
+                    GetCurrentLocation1.Text = ($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            if (cts != null && !cts.IsCancellationRequested)
+                cts.Cancel();
+            base.OnDisappearing();
+        }
+
+    }
+}
